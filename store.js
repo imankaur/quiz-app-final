1b1e7b93d85e3f253859f60ceca9e2a55ef9289a
imankaur <52762058+imankaur@users.noreply.github.com>
2019-10-22 17:13:20 -0700

    Add files via upload

---
+++ store.js
@@ -0,0 +1,57 @@
+const STORE = {
+  questions: [//1
+    {
+      question: "Which state was once part of Mexico?",
+      options: [
+        "Texas", 
+        "Maryland", 
+        "New York", 
+        "Alaska"
+      ],
+      answer: "Texas"
+    },
+      
+    //2
+    {
+      question: "What is the number of states in USA?",
+      options: [
+        "25",
+        "30", 
+        "50",
+        "52"
+      ],
+      answer: "50"
+    },
+    //3
+    {
+      question: "Which is the first 24 hours new channel?",
+      options: [
+        "CNN", 
+        "NBC", 
+        "Disney", 
+        "Star News"
+      ],
+      answer: "CNN"
+    },
+    //4
+    {
+      question: "Which is the 49th state of USA?	", 
+      options: ["Alaska", "California", "New York", "Michigan"],
+      answer: "Alaska"
+    },
+    //5
+    {
+      question: "In which year man stepped  on the moon for the first time?",
+      options: [
+        "1969", 
+        "1960", 
+        "1972", 
+        "1950"
+      ],
+      answer: "1969"
+    },
+    
+  ],
+  currentQuestion: 0,
+  score: 0
+};
